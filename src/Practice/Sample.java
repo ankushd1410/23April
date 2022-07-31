f1dbe64347d4d137ba4f7dad0e40f9179e23d409
Shree <Shree@Shree-PC>
2022-07-31 23:32:38 +0530

    Add on GitHub

---
+++ src/Practice/Sample.java
@@ -0,0 +1,29 @@
+package Practice;
+
+public class Sample {
+public static void main(String[] args) {
+		
+		//*
+		//**
+		//***
+		//****
+		//*****
+		
+		int star=1;  //2
+		
+		for(int i=1; i<=5; i++)  //for row
+		{
+			for(int j=1; j<=star; j++)
+			{
+				System.out.print("*");
+			}
+			
+			System.out.println();
+			star++;
+		}
+		
+		
+	
+	}
+
+}
